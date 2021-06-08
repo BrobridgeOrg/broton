981c684221ada42b371594f564e7f0c8c5866be0
Fred Chien <cfsghost@gmail.com>
2021-06-08 15:10:53 +0800

    take off rocksdb

--- go.mod
+++ go.mod
@@ -4,9 +4,5 @@ go 1.15
 
 require (
 	github.com/cockroachdb/pebble v0.0.0-20210605001512-cbda11b8689f
-	github.com/facebookgo/ensure v0.0.0-20200202191622-63f1cf65ac4c // indirect
-	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
-	github.com/facebookgo/subset v0.0.0-20200203212716-c811ad88dec4 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
-	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c
 )
--- options.go
+++ options.go
@@ -1,45 +1,10 @@
 package broton
 
-import "github.com/tecbot/gorocksdb"
-
 type Options struct {
-	DatabasePath   string
-	RocksdbOptions *gorocksdb.Options
+	DatabasePath string
 }
 
 func NewOptions() *Options {
 
-	// Well, I am not really sure what i am writing right here. hope it won't get any troubles. :-S
-	options := gorocksdb.NewDefaultOptions()
-	options.SetCreateIfMissing(true)
-	options.SetEnablePipelinedWrite(true)
-	options.SetAllowConcurrentMemtableWrites(true)
-	options.SetOptimizeFiltersForHits(true)
-	options.SetNumLevels(4)
-
-	blockBasedTableOptions := gorocksdb.NewDefaultBlockBasedTableOptions()
-	blockBasedTableOptions.SetBlockSizeDeviation(5)
-	blockBasedTableOptions.SetBlockSize(4 * 1024)
-	blockBasedTableOptions.SetCacheIndexAndFilterBlocks(true)
-	blockBasedTableOptions.SetCacheIndexAndFilterBlocksWithHighPriority(true)
-	blockBasedTableOptions.SetPinL0FilterAndIndexBlocksInCache(true)
-	//blockBasedTableOptions.SetIndexType(gorocksdb.KHashSearchIndexType)
-	options.SetBlockBasedTableFactory(blockBasedTableOptions)
-
-	env := gorocksdb.NewDefaultEnv()
-	env.SetBackgroundThreads(4)
-	options.SetMaxBackgroundCompactions(4)
-	options.SetTargetFileSizeBase(64 * 1024 * 1024)
-	options.SetMaxWriteBufferNumber(3)
-	options.SetLevel0FileNumCompactionTrigger(8)
-	options.SetLevel0SlowdownWritesTrigger(17)
-	options.SetLevel0StopWritesTrigger(24)
-	options.SetMaxBytesForLevelBase(512 * 1024 * 1024)
-	options.SetMaxBytesForLevelMultiplier(8)
-	options.SetMaxOpenFiles(-1)
-	options.SetEnv(env)
-
-	return &Options{
-		RocksdbOptions: options,
-	}
+	return &Options{}
 }
